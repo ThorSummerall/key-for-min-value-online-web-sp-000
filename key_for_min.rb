7abd15af8ce89b8fd8960debe42adbf9346db531
Thor Summerall <thor.summerall@gmail.com>
2020-11-22 16:50:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
-  
+
   name_hash.each do |key, value|
     if min_value == nil || value < min_value
       min_key = key
       min_value = value
   end
-  
+
   min_value
 end
